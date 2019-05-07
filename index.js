0e54d3875b4adf35c2e053b9e533481aa001cc4d
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-07 16:30:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction();
-  if (theFunk === funkyFunction){
+function theFunk(){
+  if (theFunk === funkyFunction()){
   let theFunk = 'FUNKY!';
   console.log(theFunk);
   }
+}
